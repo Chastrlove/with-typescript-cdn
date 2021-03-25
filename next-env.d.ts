fc98a33ddcab2ca8bc720b41618075424bfe4d30
马晨杰 <309272985@qq.com>
2021-03-25 09:43:22 +0800

    test

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,8 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare module "*.jpg" {
+    const src: string;
+    export default src;
+}
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,17 @@
+import React from "react";
+import Image from "next/image";
+import src  from '../resouce/taozhaiquan.jpg'
+
 const Test3 = () => {
-    return <h1>123now Client Test</h1>
+    return <>
+        <Image src={src}   priority={true}
+               height={264}
+               width={416}
+               layout={"fixed"}/>
+        <h1>123now Client Test</h1>
+    </>
+
+
 }
 
 export async function getStaticProps() {
